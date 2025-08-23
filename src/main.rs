e2a115f04358a533f379e2975e66174348e0ed71
ChaosNyaruko <cabbageparadise@gmail.com>
2025-08-23 11:32:16 +0800

    stage1, but refactor and use clap crate

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,37 @@ use std::env;
 #[allow(unused_imports)]
 use std::fs;
 
-fn main() {
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    init: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Init a git repo
+    Init,
+}
+
+fn main() -> Result<(), std::io::Error> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory")
+        }
+        unknown => println!("unknown command: {unknown}"),
+    }
+
+    Ok(())
 }
